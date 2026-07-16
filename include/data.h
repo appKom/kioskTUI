54285b26c4e5658ba740ed0be6c046c1cd602e61
Joel Neuhaus-Khatami <joel@neuhauskhatami.com>
2026-07-16 16:41:19 +0200

    add new definition to data.h

--- include/data.h
+++ include/data.h
@@ -27,7 +27,9 @@ int data_daily_count(void);
 int data_daily_get(int day_idx, const char *name);
 int data_daily_time(int day_idx, long *out_time);
 
+int data_trend(const char *name);
 int data_latest_purchase(long *ts_out, PurchaseItem *items_out, int max_items);
 int data_purchase_count(void);
+
 /* Invalidates all in-memory caches, forcing a fresh DB read on next access. */
 void data_reload(void);
